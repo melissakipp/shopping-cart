0872bff2070de1034226b0f0c72fed1ccf8a6ac3
Melissa Kipp <melissakipp.az@gmail.com>
2021-07-06 08:15:19 -0700

    fixed error for the router

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const usersRepo = require('../../repositories/users');
 
-const router = express.Route();
+const router = express.Router();
 
 router.get('/', (req, res) => {
   res.send(`
