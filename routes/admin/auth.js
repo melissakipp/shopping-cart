6d5807ea551c240bf45816729f38ea6260cef6e0
Melissa Kipp <melissakipp.az@gmail.com>
2021-07-06 09:55:23 -0700

    add custom validation

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
+
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/signup');
 const signinTemplate = require('../../views/admin/signin');
@@ -16,20 +18,39 @@ router.get('/signup', (req, res) => {
   res.send(signupTemplate({ req }));
 });
 
-router.post('/signup', async (req, res) => {
-  // Destruct
-  const { email, password, confirmPassword } = req.body;
-  const existingUser = await usersRepo.getOneBy({ email });
-  if (existingUser) {
-    return res.send('This email has been regrister already');
-  }
-  if (password !== confirmPassword) {
-    return res.send('Password must match');
-  }
-  const user = await usersRepo.create({ email, password });
-  req.session.userId = user.id;
-  res.send('Account created');
-});
+router.post('/signup', [
+  check('email')
+    .trim()
+    .normalizeEmail()
+    .isEmail()
+    .withMessage('Must be a valid email')
+    .custom(async (email) => {
+      const existingUser = await usersRepo.getOneBy({ email });
+      if (existingUser) {
+        throw new Error('Email in use');
+      }
+    }),
+  check('password')
+    .trim().isLength({ min: 4, max: 20 })
+    .withMessage('Must have at least 4 characters and less than 20 characters'),
+  check('confirmPassword')
+    .trim()
+    .isLength({ min: 4, max: 20 })
+    .custom((confirmPassword, { req }) => {
+      if (confirmPassword !== req.body.password) {
+        throw new Error('Passwords must match');
+      }
+    })
+],
+  async (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors)
+    const { email, password, confirmPassword } = req.body;
+
+    const user = await usersRepo.create({ email, password });
+    req.session.userId = user.id;
+    res.send('Account created');
+  });
 
 router.get('/signin', (req, res) => {
   res.send(signinTemplate());
